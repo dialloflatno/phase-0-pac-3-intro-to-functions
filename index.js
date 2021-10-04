8a276510709bacce01ab3cb1c8126609c70b0a98
dialloflatno <brahimdiallo23@gmail.com>
2021-10-04 15:42:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Follow along with the examples here
+//function doNothing() {}
+    
+//*function sayHello() {
+    //console.log('Hello!');
+//
+  //sayHello();
+ 
+    //ISABEL//
+    //function sayHelloToIsabel(){mconsole.log("Hello,Isable!");}sayHelloToIsabel();
+
+    //SOFIA//
+    //function sayHelloToSofia(){ console.log("Hello,Sofia!");} sayHelloToSofia();//
+
+    //BRENDA//
+    //function sayHelloToBrenda(){ console.log("Hello,Brenda");} sayHelloToBrenda();//
+
+ //A/P//
+
+ function doSomething(thing) {
+    console.log(thing);
+  }
+
+doSomething(); // passing the argument 'anything' into our function
+
+//function sayHelloTo(firstName) { console.log(`Hello, ${firstName}!`);} sayHelloTo("Isabel"); // "Hello, Isabel!"sayHelloTo("Jane"); // "Hello, Jane!"sayHelloTo("R2-D2"); // "Hello, R2-D2!"sayHelloTo(1); 
+//function say(greeting, firstName) { console.log("firstName: ", firstName); console.log("greeting: ", greeting);console.log(`${greeting}, ${firstName}!`);}//
+
+//function add(x, y) { return x + y ; } console.log(add(1, 2));//
+
+    function say(greeting, firstName) {
+        console.log(`${greeting}, ${firstName}!`);
+      }
+  
